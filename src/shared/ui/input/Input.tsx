608a86bc16525fa826a3fb4be374bab7ef76a817
GorunowMaxim <mgorunow@gmail.com>
2024-03-28 13:25:35 +0400

    rework

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,9 +1,11 @@
-import { InputHTMLAttributes, forwardRef } from "react";
+import { InputHTMLAttributes, forwardRef } from 'react';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const Input: React.FC = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
 	return <input ref={ref} type={type} {...props} className={`input-base ${className}`} />;
 });
+
+Input.displayName = 'Input';
 
 export default Input;
