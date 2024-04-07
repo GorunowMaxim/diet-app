9530724c0c89e57659cea773f5d6945def23bf10
GorunowMaxim <mgorunow@gmail.com>
2024-04-07 22:12:21 +0400

    refactor code

--- src/features/updateNutritions/ui/UpdateNutritions.tsx
+++ src/features/updateNutritions/ui/UpdateNutritions.tsx
@@ -1,7 +1,51 @@
+'use client';
+
 import { LinearProgress } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, ReactNode, useState } from 'react';
 
 import './styles.scss';
+
+const UpdateNutritionsContainer = ({ children, name }: { children: ReactNode; name: string }) => {
+	return (
+		<div className='nutritions-block'>
+			<div className='nutritions-block__wrapper'>
+				<p className='nutritions-block__progressbar-name'>{name}</p>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+interface UpdateNutritionsInputProps {
+	isChanging: boolean;
+	inputChangeValue: (e: ChangeEvent<HTMLInputElement>) => void;
+	nutritionValue: number;
+	maxNutritionValue: number;
+	handleChangeValue: () => void;
+}
+
+const UpdateNutritionsInput = ({
+	isChanging,
+	inputChangeValue,
+	nutritionValue,
+	maxNutritionValue,
+	handleChangeValue,
+}: UpdateNutritionsInputProps) => {
+	return (
+		<div className='nutritions-block__change-value'>
+			{isChanging ? (
+				<input onChange={inputChangeValue} type='number' className='nutritions-block__input' />
+			) : (
+				<div className='nutritions-block__value'>
+					{nutritionValue}/{maxNutritionValue}
+				</div>
+			)}
+			<button onClick={handleChangeValue} className='nutritions-block__button'>
+				<img className='nutritions-block__icon' src='/images/pencil.svg' alt='change-button' />
+			</button>
+		</div>
+	);
+};
 
 export const UpdateNutritions = ({ name }: { name: string }) => {
 	const [nutritionValue, setNutritionValue] = useState<number>(0);
@@ -19,31 +63,23 @@ export const UpdateNutritions = ({ name }: { name: string }) => {
 	};
 
 	return (
-		<div className='nutritions-block'>
-			<div className='nutritions-block__wrapper'>
-				<p className='nutritions-block__progressbar-name'>{name}</p>
-				<div className='nutritions-block__change-value'>
-					{isChanging ? (
-						<input onChange={inputChangeValue} type='number' className='nutritions-block__input' />
-					) : (
-						<div className='nutritions-block__value'>
-							{nutritionValue}/{maxNutritionValue}
-						</div>
-					)}
-					<button onClick={handleChangeValue} className='nutritions-block__button'>
-						<img className='nutritions-block__icon' src='/images/pencil.svg' alt='change-button' />
-					</button>
-				</div>
-				<LinearProgress
-					color='info'
-					className='nutritions-block__progressbar'
-					variant='determinate'
-					value={nutritionValue}
-				/>
-				{nutritionValue === maxNutritionValue ? (
-					<img className='nutritions-block__icon' src='/images/check.svg' alt='change-button' />
-				) : null}
-			</div>
-		</div>
+		<UpdateNutritionsContainer name={name}>
+			<UpdateNutritionsInput
+				isChanging={isChanging}
+				inputChangeValue={inputChangeValue}
+				nutritionValue={nutritionValue}
+				maxNutritionValue={maxNutritionValue}
+				handleChangeValue={handleChangeValue}
+			/>
+			<LinearProgress
+				color='info'
+				className='nutritions-block__progressbar'
+				variant='determinate'
+				value={nutritionValue}
+			/>
+			{nutritionValue === maxNutritionValue ? (
+				<img className='nutritions-block__icon' src='/images/check.svg' alt='change-button' />
+			) : null}
+		</UpdateNutritionsContainer>
 	);
 };
