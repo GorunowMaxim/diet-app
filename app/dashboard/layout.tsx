6b0ac01d9aa4351136f8e48c10ff21bff478337f
GorunowMaxim <mgorunow@gmail.com>
2024-05-22 18:58:16 +0400

    delete redux

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,17 +1,8 @@
 import { Toaster } from 'react-hot-toast';
-import { Inter } from 'next/font/google';
-import type { Metadata } from 'next';
 
 import { SideBar } from '@/widgets/sideBar';
 
 import './styles.scss';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-	title: 'Diet app',
-	description: 'Special app was creating to help people solve their problems with weight',
-};
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
 	return (
